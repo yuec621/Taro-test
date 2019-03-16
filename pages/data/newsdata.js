70a67ecb3d289d69f300a7c05130d1c87fffcaee
yuec621 <yuec621@outlook.com>
2019-03-16 16:10:06 +0800

    初始化

---
+++ pages/data/newsdata.js
@@ -0,0 +1,17 @@
+var newsData=[
+    {
+        "newsid":0,
+        "authorIcon":"../images/head1.png",
+        "authorName":"iwen",
+        "authorDate":"2017/3/1",
+        "title":"岁月不老  我们不散",
+        "artcleImg":"../images/sub1.png",
+        "artcleText":"你好世界  岁月不老  我们不散",
+        "artcleLikeComment":"12",
+        "artcleLikeSC":"30"
+
+    }
+]
+module.exports={
+    newsData:newsData
+}
